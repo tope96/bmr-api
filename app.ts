670d2562d9e8e5713aaee96704572e431d2f534e
tope96 <pietrzaktomek@hotmail.com>
2021-11-28 12:45:46 +0100

    Add request limiter

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {routes} from './src/routes'
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const PORT = 3000;
+
+//100 per 15 minutes
+const apiRateLimit = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+})
+
+app.use(apiRateLimit)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
